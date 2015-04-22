adcfe045a0876268d7dbd8ef302a9cfe562d8770
buddhi1 <buddhiashan1@gmail.com>
2015-04-22 16:56:47 +0530

    dayoff4223

--- app/views/admin/doctor/add.blade.php
+++ app/views/admin/doctor/add.blade.php
@@ -1,7 +1,29 @@
 @extends('admin.layouts.main')
 
 @section('content')
-{{ Form::open(array('url'=>'admin/doctor/create')) }}
-{{ Form::label('lblname', 'Doctor name') }}
+{{ Form::open(array('url'=>'admin/doctor/create', )) }}
+
+@if(Session::has('message'))
+	<div>{{ Session::get('message') }}</div>
+@endif
+@if($errors->has())
+<div>
+	<ul>
+		@foreach($errors->all() as $error)
+			<li> {{ $error }} </li>
+		@endforeach
+	</ul>
+</div>	
+@endif
+
+<div>{{ Form::label('lblname', 'Doctor name') }}: {{ Form::text('name') }}</div>
+<div>{{ Form::label('lblname', 'Description') }}: {{ Form::textarea('description') }}</div>
+<div>{{ Form::label('lblname', 'Experience') }}: {{ Form::textarea('experience') }}</div>
+<div>{{ Form::label('lblname', 'Contact No.') }}: {{ Form::text('tp') }}</div>
+<div>{{ Form::label('lblname', 'Special message') }}: {{ Form::textarea('special') }}</div>
+<div>{{ Form::label('lblname', 'Email') }}: {{ Form::email('email') }}</div>
+<div>{{ Form::label('lblname', 'Passowrd') }}: {{ Form::password('password') }}</div>
+<div>{{ Form::submit('Add doctor') }}</div>
+
 {{ Form::close() }}
 @stop
