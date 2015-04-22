c2e2b4f979b38d83d34bfe2ed284b4169ba289ae
buddhi1 <buddhiashan1@gmail.com>
2015-04-22 13:44:56 +0530

    alter user table type migration

--- app/database/migrations/2015_04_22_070948_alter_user_table_type.php
+++ app/database/migrations/2015_04_22_070948_alter_user_table_type.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterUserTable extends Migration {
+class AlterUserTableType extends Migration {
 
 	/**
 	 * Run the migrations.
