0929dfa9bb2468b2b642ac80f1f750079dfb4f27
pulasthilakshan <pulasthilakshan@gmail.com>
2015-04-22 12:38:03 +0530

    locations finished

--- app/controllers/StateController.php
+++ app/controllers/StateController.php
@@ -45,13 +45,76 @@ class StateController extends BaseController{
 	}
 
 	public function postDestroy() {
-	// delete a state
+	// delete a State
+
+		$id = Input::get('id');
+		$all_towns = array();
+		$all_lgas = array();
+
+		$state = State::find($id);
+
+		if($state) {
+
+			$lgas = DB::table('lgas')->where('state_id', '=', $state->id)->get();
+
+			if($lgas) {
+
+				foreach ($lgas as $lga) {
+
+					$all_lgas[] = $lga->name;
+
+					$towns = DB::table('towns')->where('lga_id', '=', $lga->id)->get();
+
+					if($towns) {
+
+						foreach ($towns as $town) {
+
+							$all_towns[] = $town->name;
+						}
+					}
+				}
+
+				return View::make('admin.state.delete')
+					->with('all_towns', $all_towns)
+					->with('all_lgas', $all_lgas)
+					->with('state_id', $state->id);
+			}
+
+			$state->delete();
+
+			return Redirect::To('admin/state')
+				->with('message', 'State Successfully Deleted');
+		}
+
+		return Redirect::To('admin/state')
+			->with('message', 'Cannot Delete the State');
+	}
+
+	public function postDestroyall() {
+	// delete all LGA
 
 		$id = Input::get('id');
 
 		$state = State::find($id);
 
 		if($state) {
+
+			while($lga_dat = DB::table('lgas')->where('state_id', '=', $state->id)->first()) {
+				$lga = Lga::find($lga_dat->id);
+
+				if($lga) {
+
+					while($town_dat = DB::table('towns')->where('lga_id', '=', $lga->id)->first()) {
+						$town = Town::find($town_dat->id);
+
+						if($town) {
+
+							$town->delete();
+						}
+					}
+					$lga->delete();
+				}
+			}
 
 			$state->delete();
 
