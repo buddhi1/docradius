d756af7d5fadc65e06a7cf597a0cd700677103dc
pulasthilakshan <pulasthilakshan@gmail.com>
2015-04-24 16:49:40 +0530

    Health job started

---
+++ app/controllers/JobController.php
@@ -0,0 +1,45 @@
+<?php
+
+class JobController extends BaseController {
+
+	public function __construct() {
+
+		$this->beforeFilter('csrf', array('on' => 'post'));
+	}
+
+	public function getCreate() {
+		// display the create form for a Job
+
+		return View::make('member.job.add');
+	}
+
+	public function postCreate() {
+		// save health job in the database
+
+		$title = Input::get('title');
+		$des = Input::get('des');
+		$email_admin = Input::get('email');
+		$email = 'pulasthilakshan@gmail.com';	//Auth::user('email');
+		$user_id = '4';		//Auth::user('id');
+
+		$job = new Job;
+
+		if($job) {
+
+			$job->title = $title;
+			$job->description = $des;
+			if(!$email_admin) {
+				$job->email = $email;
+			} else {
+				$job->email = $email_admin;
+			}
+			$job->user_id = $user_id;
+
+			if($job->save()) {
+
+				return Redirect::To('member/job/create')
+					->with('message', 'Health Job is Created');
+			}
+		}
+	}
+}
---
+++ app/views/member/job/add.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.layouts.main')
+
+@section('content')
+
+@if(Session::has('message'))
+	<div>{{ Session::get('message') }}</div>
+@endif
+@if($errors->has())
+	<div class="alert alert-danger">
+		<ul>
+			@foreach($errors->all() as $error)			
+					<li> {{ $error }} </li>			
+			@endforeach
+		</ul>
+	</div>
+@endif
+
+{{ Form::open(array('url'=>'member/job/create')) }}
+
+<div>{{ Form::label('title', 'Title') }}: {{ Form::text('title', null, array('required')) }}</div>
+@if(Auth::user('type' !== 1))
+<div>{{ Form::label('email', 'Email') }}: {{ Form::text('email', null, array('required')) }}</div>
+@endif
+<div>{{ Form::label('des', 'Description') }}: {{ Form::textarea('des', null, array('required')) }}</div>
+<div> {{ Form::submit('Create') }} </div>
+
+{{ Form::close() }}
+@stop
