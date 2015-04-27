54e9235bb3e940d7108259f0813744d2b2ed9d2c
buddhi1 <buddhiashan1@gmail.com>
2015-04-27 16:59:20 +0530

    day of  427

---
+++ app/views/emails/auth/cancellation.blade.php
@@ -0,0 +1,10 @@
+Dear {{$name}}, <br><br>
+Your appoinment for Dr.{{$doc_name}} on {{$channel->chanelling_date}} at {{$channel->time}} has been cancelled due to an unavoidable circumstances.<br>
+Please try to schedule another appoinment on different time slot.<br>
+Keep in touch with us.
+
+<br><br><br>
+---------------------------------------------------------------------<br>
+System generated message by Docradius<br>
+---------------------------------------------------------------------<br>
+------------------- powered by Ingens -------------------
